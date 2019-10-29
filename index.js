8f588b92a7cdd2bb8588b0a34f6e4b7ad03671d6
Madhav <36961049+madhav3009@users.noreply.github.com>
2019-10-29 18:34:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const port = 8000;
 
 const db = require('./config/mongoose');
 
-const Contact = require('./models/contact').default;
+// const Contact = require('./models/contact').default;
+const Contact = require('./models/contact');
+
 
 const app = express();
 
@@ -21,59 +23,60 @@ var contactList = [
 ]
 
 
-app.listen(port, function(err){
-    if (err) {
-        console.log("Error in running server", err);
-    }
-    console.log('My Server running on Port', port);
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error in running server", err);
+  }
+  console.log('My Server running on Port', port);
 })
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 
+  Contact.find({}, function (err, contacts) {
+    if (err) {
+      console.log("error in fetching contacts from db");
+      return;
+    }
+    return res.render('home', {
+      title: "Phone Contact List",
+      contact_list: contacts
+    });
 
-app.post('/create-contact', function(req, res){
-    
-    Contact.find({}, function(err, contacts){
-        if(err){
-            console.log("error in fetching contacts from db");
-            return;
-        }
-        return res.render('home',{
-            title: "Phone Contact List",
-            contact_list: contacts
-        });
-    
-    })
-    
-    })
-
-    
-    Contact.create({
-        name: req.body.name,
-        phone: req.body.phone,
-        email:req.body.email
-    }, function(err, newContact){
-        if(err){
-            console.log('Error in creating a contact!')
-            return;
-             }
-            return res.redirect('back');
-    })
-
+  })
 });
 
 
-app.get('/delete-contact/', function(req, res){
-    console.log(req.query);
-    let id = req.query.id
+app.post('/create-contact', function (req, res) {
+  Contact.create({
+    name: req.body.name,
+    phone: req.body.phone,
+    email: req.body.email
+  }, function (err, newContact) {
+    if (err) {
+      console.log('Error in creating a contact!')
+      return;
+    }
+    return res.redirect('back');
+  })
 
-    Contact.findOneAndDelete(id, function(err){
-        if(err){
-            console.log('error in deleting the object');
-            return;
-        }
-        return res.redirect('back');
-    })
-  
+})
+
+
+
+
+
+
+app.get('/delete-contact/', function (req, res) {
+  console.log(req.query);
+  let id = req.query.id
+
+  Contact.findOneAndDelete(id, function (err) {
+    if (err) {
+      console.log('error in deleting the object');
+      return;
+    }
+    return res.redirect('back');
+  })
+
 });
